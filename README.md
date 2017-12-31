5b1720b599ebc57978b60cc277ebfe461c326f4d
LauraJH16 <laurajimenezh16@hotmail.com>
2017-12-31 01:56:00 -0500

    Proyecto Terminado

---
+++ README.md
@@ -0,0 +1,31 @@
+# Reto - Lyft
+
+## Objetivo:
+
+Vista splash con duración de 2 a 5 segundos que redirecciona a tu vista de inicio. La vista de inicio cuenta con dos botones, en esta ocasión seguiremos el flujo de SING UP. 
+
+![Objetivo-1](assets/docs/splash-obj.png)
+
+En la siguiente vista tenemos un formulario donde nuestro usuario puede escoger el país y debe ingresar su número de teléfono. El botón de NEXT debe estar deshabilitado hasta que se ingrese un número de 10 dígitos. 
+
+![Objetivo-2](assets/docs/numero-obj.png)
+
+Una vez ingresado el número de teléfono se habilita el botón y al dar click debe enviar una alerta con un código generado aleatoriamente (LAB-000) y redireccionar a la siguiente vista.
+
+![Objetivo-3](assets/docs/alert-obj.png)
+
+En esta vista se debe ingresar el código dado anteriormente y una vez hecho esto se habilita el botón que redirecciona a nuestro usuario a la vista donde ingresa sus datos. (Puede tener la opción de enviar otro código.)
+
+![Objetivo-4](assets/docs/codigo-obj.png)
+
+Para ingresar sus datos necesitamos un formulario que le pida su nombre, apellido y correo electrónico. Deberá también tener un checkbox para que se acepten los términos y condiciones del servicio. 
+
+![Objetivo-5](assets/docs/nombre-obj.png)
+
+Ya que se ha realizado lo anterior, sólo se deberá mostrar una vista al usuario que le indique que ha concluido con el registro exitosamente. 
+
+![Objetivo-6](assets/docs/final-obj.png)
+
+## Autora: 
+
+Laura Jimenez Hidalgo.
---
+++ js/app.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $('#splash').css('display', 'none');
+    $('#home').css('display', 'block');
+  }, 3000);
+
+  var signUp = $('#sign-up');
+  var x = Math.floor((Math.random() * (999 - 99 + 1)) + 99);    
+  var validateEmail = false;
+  var validateName = false; 
+  var validateLastName = false;
+  var validateCheck = false;
+  var $check = $('input[type=checkbox]');
+
+  $(signUp).click(function() {
+    window.location.href = 'views/sign1.html';
+  });
+
+  $('.chevron').click(function() {
+    window.location.href = '../index.html';        
+  });
+
+  $('select').material_select();
+
+  $('.select-country').click(function() {
+    $('ul').toggleClass('desactive');
+    $('ul').toggleClass('activelist');
+  });
+
+  $('.btn-sign').click(function() {
+    alert('Tú codigo: LAB-' + x);
+    window.location.href = 'sign2.html';
+  });
+
+  $('#sign input').keyup(function() {
+    if ($('#sign input').val().length > 8) {
+      $('.btn-sign').removeAttr('disabled');
+      $('.btn-sign').removeClass('disable');            
+    } else {
+      $('.btn-sign').prop('disabled', true); 
+      $('.btn-sign').addClass('disable');
+    }
+  });
+
+  $('.chevron-two').click(function() {
+    window.location.href = 'sign1.html';
+  });
+
+  $('.resend').click(function() {
+    alert('Tú codigo: LAB-' + x);        
+  });
+
+  $('#sign .input-two').keyup(function() {
+    if ($('#sign .input-two').val().length > 2 && $('#sign .input-two').val().length < 4) {
+      $('.btn-sign-two').removeAttr('disabled');
+      $('.btn-sign-two').removeClass('disable');            
+    } else {
+      $('.btn-sign-two').prop('disabled', true); 
+      $('.btn-sign-two').addClass('disable');
+    }
+  });
+
+  $('.btn-sign-two').click(function() {
+    window.location.href = 'form.html';
+  });
+
+  $('.chevron-three').click(function() {
+    window.location.href = 'sign2.html';
+  });
+
+  $('.email-form').keyup(function(event) {
+    var regexEmail = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+
+    if (regexEmail.test($(this).val())) {
+      validateEmail = true;
+      enableButton(); 
+    } else {
+      disableButton();
+    }
+  });
+
+  $('.name-form').keyup(function(event) {
+    if ($(this).val().length > 3 && $(this).val() !== '') {
+      validateName = true;
+      enableButton(); 
+    } else {
+      disableButton();
+    }
+  });
+
+  $('.lastname-form').keyup(function(event) {
+    if ($(this).val().length > 3 && $(this).val() !== '') {
+      validateLastName = true;
+      enableButton(); 
+    } else {
+      disableButton();
+    }
+  });
+
+  $check.on('click', function(event) {
+    if (event.target.checked) {
+      validateCheck = true;
+      enableButton();
+    } else {
+      disableButton();
+    }
+  });
+
+  function enableButton() {
+    if (validateName && validateLastName && validateEmail && validateCheck) {
+      $('.btn-sign-three').removeAttr('disabled');
+      $('.btn-sign-three').removeClass('disable'); 
+    }
+  }
+    
+  function disableButton() {
+    $('.btn-sign-three').prop('disabled', true); 
+    $('.btn-sign-three').addClass('disable');
+  }
+
+  $('.btn-sign-three').click(function() {
+    window.location.href = 'finale.html';
+  });
+});
